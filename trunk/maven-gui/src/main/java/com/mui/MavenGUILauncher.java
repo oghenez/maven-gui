e85bef2853c194eceb2a9cd122f9a0a1e5b69cfd
sabuj.das <sabuj.das@29159b88-29b2-11de-8010-b76041e17fd7>
2009-04-30 11:31:24 +0000

    The architecture of the project is changed

---
+++ trunk/maven-gui/src/main/java/com/mui/MavenGUILauncher.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mui;
+
+/**
+ *
+ * @author sabuj.das
+ */
+public class MavenGUILauncher {
+
+    public static void main(String[] args){
+        
+    }
+}
